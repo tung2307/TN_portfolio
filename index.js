d6b16a443bc7789cc6b3f8d1ab9916142f6dd02e
Tung Nguyen <tungnguyen@sfs-wifi-dhcp-10-143-246-41.wifi.sfsu.edu>
2023-03-07 13:42:13 -0800

    Fix contact form

--- index.js
+++ index.js
@@ -1,20 +1,14 @@
-var host = config.HOST;
-var username = config.USERNAME;
-var password = config.PASSWORD;
-var email = document.getElementById("email").value;
-var subject = document.getElementById("subject").value;
-var message = document.getElementById("message").value;
-
 function sendEmail() {
 	Email.send({
-	Host: host,
-	Username : username,
-	Password : password,
-	To : 'tung.nguyen23797@gmail.com',
-	From : email,
-	Subject : subject,
-	Body : message,
+	SecureToken: "936d8007-9806-4dda-953d-fef15f828fe9",
+	From : 'tung.nguyen23797@gmail.com',
+	To : "n13a10tt31.ngtung@gmail.com",
+	Subject : "New Contact Form Enquiry",
+	Body : "Subject: " + document.getElementById("subject").value
+		  + "<br> Name: " + document.getElementById("name").value
+		  + "<br> Email: " + document.getElementById("email").value
+		  + "<br> Email: " + document.getElementById("message").value
 	}).then(
-		message => alert("mail sent successfully")
+	  message => alert("Message Sent Succesfully")
 	);
-}
+  }
